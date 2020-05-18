1bda3a9af883dac675365eddbd095bc70c99c9d6
Victor Fung <victor13021997@gmail.com>
2020-05-18 17:06:16 +0800

    Update grab credit transfer info method - the thing have more info now - the automatic date function is deleted as the website have done it - combine.js: now grab the code represent the Uni in the website - combine.json: add a properties called "creditCode" - ust_info_scraper: delete the old method and apply the new method

--- Accommodation json/combine.js
+++ Accommodation json/combine.js
@@ -1,4 +1,6 @@
 const fs = require("fs");
+const request = require("request");
+const cheerio = require("cheerio")
 
 let linkFile = fs.readFileSync('accommodation.json');
 let linkJSON = JSON.parse(linkFile);
@@ -12,47 +14,61 @@ let locationJSON = JSON.parse(locationFile);
 //     return { id: index, name: val.name }
 // })
 
+function clean(words) {
+    words = words.replace(/&/g, "and");
+    words = words.replace(/the/g, "");
+    words = words.replace(/The/g, "");
+    words = words.replace(/-/g, "");
+    if (words.includes("University of Illinois,")) {
+        words = words.substring(0, words.indexOf(","));
+    }
+    if (words.includes("Stony Brook"))
+        words = "Stony Brook";
+    words = words.replace(/,/g, "");
+    words = words.replace(/ /g, "");
+    words = words.toLowerCase();
+    return words;
+}
+
 locationJSON.forEach(element => {
     // let uniName = {id: element.id, locName: element.name.replace("--", ", ")}
     let uniName = element.name.replace("--", ", ")
-    
+
     // let hasName = (element) => (element.name.localeCompare(uniName.locName) == 0)? true : false;
-    let hasName = (element) => (element.name.localeCompare(uniName) == 0)? true : false;
-    
+    let hasName = (element) => (element.name.localeCompare(uniName) == 0) ? true : false;
+
     // 1st try: just match
-    if(linkJSON.findIndex(hasName) !== -1){
+    if (linkJSON.findIndex(hasName) !== -1) {
         // uniName.linkName = linkJSON[linkJSON.findIndex(hasName)].name;
         element.ustName = linkJSON[linkJSON.findIndex(hasName)].name;
         element.link = linkJSON[linkJSON.findIndex(hasName)].link;
         element.accommodation_res = linkJSON[linkJSON.findIndex(hasName)].results;
     }
-    else 
-    {
+    else {
         // 2nd try: match the name without things after ","
         // let hasNameRed = (element) => (element.name.localeCompare(uniName.locNameRed) == 0) ? true : false;
-        
+
         // uniName.locNameRed = uniName.locName.substring(0,uniName.locName.indexOf(","));
-        uniName = uniName.substring(0,uniName.indexOf(","));
+        uniName = uniName.substring(0, uniName.indexOf(","));
 
         // if(linkJSON.findIndex(hasNameRed) !== -1){
-        if(linkJSON.findIndex(hasName) !== -1){
+        if (linkJSON.findIndex(hasName) !== -1) {
             // uniName.linkName = linkJSON[linkJSON.findIndex(hasNameRed)].name;
             element.ustName = linkJSON[linkJSON.findIndex(hasName)].name;
             element.link = linkJSON[linkJSON.findIndex(hasName)].link;
             element.accommodation_res = linkJSON[linkJSON.findIndex(hasName)].results;
         }
-        else
-        {
+        else {
             // 3rd try: match the things with the things before "," 
             // and delete any "University" inside the string
             // and use includes() instead of localeCompare()
-            
+
             // uniName.locNameRed = uniName.locNameRed.replace("University","").trim();
-            uniName = uniName.replace("University","").trim();
-            
+            uniName = uniName.replace("University", "").trim();
+
             linkJSON.forEach(ele => {
                 // if(ele.name.includes(uniName.locNameRed))
-                if(ele.name.includes(uniName)){
+                if (ele.name.includes(uniName)) {
                     // uniName.linkName = ele.name;
                     element.ustName = ele.name;
                     element.link = ele.link;
@@ -66,6 +82,29 @@ locationJSON.forEach(element => {
 
     // compare.push(uniName);
 });
+
+var uniList = []
+
+locationJSON.forEach(element => {
+    uniList[element.id - 1] = clean(element.ustName);
+});
+
+request.get("https://crtran.ust.hk/credit_instit", (err, res, body) => {
+    if (!err) {
+        $ = cheerio.load(body);
+        $('[label="' + "United States" + '"] option').each(function (i, elem) {
+            let uniName = clean($(this).text());
+            let compareName = (ele) => uniName.includes(ele) || ele.includes(uniName);
+            let index = uniList.findIndex(compareName);
+            if (index !== -1) {
+                locationJSON[index].creditCode = $(this).attr("value");
+            }
+        });
+        let output = JSON.stringify(locationJSON);
+        fs.writeFileSync('combine.json', output);
+    }
+});
+
 // console.log(compare);
-let output = JSON.stringify(locationJSON);
-fs.writeFileSync('combine.json', output);
+// let output = JSON.stringify(locationJSON);
+// fs.writeFileSync('combine.json', output);
--- Accommodation json/ust_info_scraper.js
+++ Accommodation json/ust_info_scraper.js
@@ -1,17 +1,19 @@
 var request = require("request");
 var cheerio = require("cheerio");
 const fs = require("fs");
-var base = 'https://crtran.ust.hk/credit_instit?asOfTermCode=' //&orgID='
+var base = 'https://crtran.ust.hk/credit_instit?orgID='//asOfTermCode=' //&orgID='
 
 let rawdata = fs.readFileSync('combine.json');
 let inputJSON = JSON.parse(rawdata);
 
+/* 
 let current = new Date();
 //          [Jan  ,  Feb,  Mar,  Apr,  May,  Jun,  Jul,  Aug,  Sep,  Oct,  Nov, Dec ]
 //         Winter | Spring              | Summer      | Fall                    | Winter
 let month = ["20" , "30", "30", "30", "40", "40", "40", "10", "10", "10", "10", "20"];
 let year = (current.getMonth() < 7) ? (current.getFullYear() - 1).toString().substr(-2, 2): current.getFullYear().toString().substr(-2, 2);
-base += year + month[current.getMonth()];
+base += year + month[current.getMonth()]; 
+*/
 
 console.log(base);
 
@@ -22,18 +24,15 @@ function clean(words) {
     words = words.replace(/the/g, "");
     words = words.replace(/The/g, "");
     words = words.replace(/-/g, "");
-    if (words.includes("University of Illinois,")){
+    if (words.includes("University of Illinois,")) {
         words = words.substring(0, words.indexOf(","));
-        console.log(words);
     }
     if (words.includes("Stony Brook"))
-    words = "Stony Brook";
+        words = "Stony Brook";
     words = words.replace(/,/g, "");
     words = words.replace(/ /g, "");
     words = words.toLowerCase();
-    if (words.includes("illinois"))
-        console.log(words);
-    return words;  
+    return words;
 }
 
 // prepare the properties of each Uni object
@@ -42,30 +41,47 @@ inputJSON.forEach(element => {
     uniList[element.id - 1] = clean(element.ustName);
 });
 
-// for generate final.json (add credit transfer info)
-request.get(base,function(err,res,body){
-    var item = {
-        exchangeCourse: "",
-        hkustCourse: ""
-    }
-    if(!err){
-        $ = cheerio.load(body);
-        $('tr').each(function(i, elem) {
-            // comparing the
-            let uniName = clean($(this).children().first().children().first().children().first().text());
-            let compareName = (ele) => uniName.includes(ele) || ele.includes(uniName);
-            let index = uniList.findIndex(compareName);
-            if (index !== -1) {
-                item = {
-                    // university name: $(this).children().first().children().first().children().first().text()
-                    exchangeCourse: $(this).children().first().children().first().children().first().next().text(),
-                    hkustCourse: $(this).children().first().next().text(),
-                    hkustCode: $(this).children().first().next().text().split(" ")[0]
-                }
-                inputJSON[index].courses.push(item);
+function getTranCourse(link, index) {
+    return new Promise(resolve => {
+        // for generate final.json (add credit transfer info)
+        console.log(link);
+        console.log(inputJSON[index].creditCode);
+        request.get(link, function (err, res, body) {
+            var item = {
+                exchangeCourse: "",
+                hkustCourse: ""
+            }
+            if (!err) {
+                $ = cheerio.load(body);
+                $('tbody tr').each(function (i, elem) {
+                    // comparing the
+                    item = {
+                        // university name: $(this).children().first().children().first().children().first().text()
+                        exchangeCourse: $(this).children().first().children().first().children().first().next().text(),
+                        hkustCourse: $(this).children().first().next().text(),
+                        hkustCode: $(this).children().first().next().text().split(" ")[0]
+                    }
+                    inputJSON[index].courses.push(item);
+                });
+                resolve(true);
             }
         });
+    })
+}
+
+function getAllTranCourse() {
+    const promises = [];
+    for (let i = 0; i < inputJSON.length; i++) {
+        promises.push(getTranCourse(base + inputJSON[i].creditCode, i));
     }
+    return Promise.all(promises);
+}
+
+let p = Promise.resolve();
+p = p.then(() => {
+    return getAllTranCourse();
+})
+p.then(() => {
     let output = JSON.stringify(inputJSON);
     fs.writeFileSync('final.json', output);
-});
+})
